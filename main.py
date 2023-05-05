ceb4614f2903cd39d9cd249a98115f772cd259e4
Zennara <64995253+Zennara@users.noreply.github.com>
2023-05-04 21:02:00 -0400

    Repeat forever and # of times

--- main.py
+++ main.py
@@ -11,11 +11,18 @@ class Interpreter:
     def interpret(self, code):
         valid_characters = "abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"
         tokens = ['save', 'as', 'if', 'equals', 'above', 'below', 'repeat', 'forever', 'until', 'times', 'and', 'or',
-                  'and', 'display']
+                  'and', 'display', 'otherwise', 'stop']
         lines = code.split("\n")
+        saved_lines = ""
+        saving = False
+        repeat_type = None
 
         for line in lines:
             split_line = re.split(r' \s*(?=(?:[^"]*"[^"]*")*[^"]*$)', line)
+
+            if saving and not line.startswith("end"):
+                saved_lines += line + "\n"
+                continue
 
             # storing variable
             if line.startswith("save "):
@@ -62,6 +69,33 @@ class Interpreter:
                     else:
                         Interpreter.error(self,
                                           "Invalid token - Tokens must only contain letters, underscores, or dashes.")
+
+            # loops
+            elif line.startswith("repeat "):
+                # forever
+                if split_line[1] == "forever":
+                    saving = True
+                    repeat_type = "forever"
+                # set amount of times
+                elif split_line[2] == "times":
+                    if split_line[1].isdigit():
+                        repeat_type = split_line[1]
+                        saving = True
+                    else:
+                        Interpreter.error(self, "Invalid token - Disallowed repeat amount")
+
+            # end if, repeat, etc
+            elif line.startswith("end "):
+                if split_line[1] == "repeat":
+                    saving = False
+                    interpreter_repeat = Interpreter()
+
+                    if repeat_type == "forever":
+                        while True:
+                            interpreter_repeat.interpret(saved_lines)
+                    elif repeat_type.isdigit():
+                        for x in range(0, int(repeat_type)):
+                            interpreter_repeat.interpret(saved_lines)
 
 
 f = open('run_me.ez', encoding="utf-8")  # open json file
